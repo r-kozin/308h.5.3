bbbf9d0c49976a8b96c1426d61b71165cdcf3549
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-06-01 13:26:56 -0700

    finished lab after getting corrections from Malek

--- index.js
+++ index.js
@@ -1,145 +1,163 @@
 const personOne = {
-    name: {
-        first: 'Timmy',
-        last: 'Timtim'
-    },
-    age: 30,
-    location: {
-        city: 'New York',
-        state: 'New York',
-        zip: 10001
-    }
-}
+  name: {
+    first: "Timmy",
+    last: "Timtim",
+  },
+  age: 30,
+  location: {
+    city: "New York",
+    state: "New York",
+    zip: 10001,
+  },
+};
 
 const personTwo = {
-    name: {
-        first: 'Julie',
-        last: 'July'
-    },
-    age: 28,
-    location: {
-        city: 'Albany',
-        state: 'New York',
-        zip: 12201
-    }
+  name: {
+    first: "Julie",
+    last: "July",
+  },
+  age: 28,
+  location: {
+    city: "Albany",
+    state: "New York",
+    zip: 12201,
+  },
+};
+
+function linkTwoObjectLocationByReference(obj, obj2, location) {
+    obj.location = location;
+    obj2.location = location;
 }
-const personThree = {    
-    name: {
-        first: '',
-        last: ''
-    },
-    age: 0,
-    location: {
-        city: '',
-        state: '',
-        zip: 0
-    }
-    }
 
 //task1
 function moveLocation(person, newLocation) {
-    person.location = newLocation;
-    if (person == personOne) {
-        personTwo.location = newLocation;
-        personThree.location = newLocation;
-    }
-    if (person == personTwo) {
-        personOne.location = newLocation;
-        personThree.location = newLocation;
+    for(const key in person.location){
+        person.location[key] = newLocation[key];
     }
 }
+
 const nLoc = {
     city: 'Rochester',
     state: 'New York',
     zip: 14604
 }
 
-console.log("Log personTwo");
-console.log(personTwo);
-
 moveLocation(personTwo, nLoc);
-console.log("Log moved personTwo");
-console.log(personTwo);
+
+console.log("Location moved");
+console.log(personTwo.location.city);
+
 
 //task2
+linkTwoObjectLocationByReference(personOne, personTwo, nLoc);
+
+console.log("PersonOne and PersonTwo linked on nLoc");
+console.log(personOne);
+console.log(personTwo);
+
 const nLoc2 = {
     city: 'Mountain View',
     state: 'California',
     zip: 94035
 }
 
+console.log("Move PersonOne after linked");
 moveLocation(personOne, nLoc2);
+
 console.log(personOne);
 console.log(personTwo);
 
 //task3
+console.log("creating person3");
+const personThree = {
+    name: {
+        first: "",
+        last: "",
+    },
+    age: 0,
+    location: {
+        city: "",
+        state: "",
+        zip: 0
+    },
+}
 
 personThree.name.first = 'Jimmy';
 personThree.name.last = personOne.name.last + '-' + personTwo.name.last;
 personThree.age = 0;
-personThree.location = personOne.location;
-
+console.log("link person3 to persontwo by reference on nLoc");
+linkTwoObjectLocationByReference(personThree, personTwo, nLoc);
 console.log(personThree);
 
+console.log("move personOne to nLoc3");
 const nLoc3 = {
-    city: 'TEST MOVE WITH BABY',
-    state: 'BABY',
-    zip: 90210
+    city: 'Shady Shores',
+    state: 'Hawaii',
+    zip: 55555
 }
 
-moveLocation(personOne, nLoc3);
-console.log(personThree);
+moveLocation(personOne, nLoc3)
+
+console.log("log all three people after move");
 console.log(personOne);
+console.log(personTwo);
+console.log(personThree);
 
-//task4
-const nLoc4 = {
-    city: 'Dontmovewithbaby',
-    state: 'NOBABY',
-    zip: 00000
-}
+//task 4
+console.log("increment all ages by 20");
 personOne.age += 20;
 personTwo.age += 20;
 personThree.age += 20;
-const p3Loc = personOne.location;
-personThree.location = p3Loc;
-moveLocation(personThree, nLoc4);
 
+console.log("create copy of personThree location and set location = to that");
+
+let locDeepCopy = JSON.parse(JSON.stringify(personThree.location));
+personThree.location = locDeepCopy;
+
+const nLoc4 = {
+    city: 'Cityville',
+    state: 'Stateville',
+    zip: 33333
+}
+
+console.log("move location of PersonThree");
+moveLocation(personThree, nLoc4)
 console.log(personThree);
-console.log(personOne);
 console.log(personTwo);
 
 //task5
 console.log("Welcome to the future!");
-
+console.log("increment all ages by +300");
 personOne.age += 300;
 personTwo.age += 300;
 personThree.age += 300;
 
 function clonePerson(person){
-    let clonedDeepCopy = JSON.parse(JSON.stringify(person));
-    clonedDeepCopy.age = 0;
-    return clonedDeepCopy;
+    let clonedPerson = JSON.parse(JSON.stringify(person));
+    clonedPerson.age = 0;
+    return clonedPerson;
 }
 
-console.log(clonePerson(personThree));
+console.log("Clone Persons 1 & 2");
+const clonedPersonOne = clonePerson(personOne);
+const clonedPersonTwo = clonePerson(personTwo);
 
-let clonedOne = clonePerson(personOne);
-let clonedTwo = clonePerson(personTwo);
+console.log(clonedPersonOne);
+console.log(clonedPersonTwo);
 
-moveLocation(clonedTwo, nLoc);
-moveLocation(clonedOne, nLoc2);
+console.log("Move clone location");
+const nLoc5 = {
+    city: 'PLACE',
+    state: 'WHERE',
+    zip: 888888
+}
+moveLocation(clonedPersonOne, nLoc4);
+moveLocation(clonedPersonTwo, nLoc5);
+console.log(clonedPersonOne);
+console.log(clonedPersonTwo);
 
-console.log(personOne.location);
-console.log(clonedOne.location);
-console.log(personTwo.location);
-console.log(clonedTwo.location);
 
-console.log(personOne.age);
-console.log(clonedOne.age);
-console.log(personTwo.age);
-console.log(clonedTwo.age);
-
-//task6
+//task 6
 
 const thoughts = {
     thought1: 'this is a though',
@@ -149,8 +167,8 @@ const thoughts = {
 personOne.thoughts = thoughts;
 personTwo.thoughts = thoughts;
 personThree.thoughts = thoughts;
-clonedOne.thoughts =thoughts;
-clonedTwo.thoughts =thoughts;
+clonedPersonOne.thoughts =thoughts;
+clonedPersonTwo.thoughts =thoughts;
 
 console.log(personOne.thoughts);
 
